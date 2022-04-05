4debf7e7015e7086ce3301d836b5ec3ca42aa647
Dien Cap <baodien1405@gmail.com>
2022-04-05 10:40:56 +0700

    Forward all reqs to api server

---
+++ components/layout/empty.tsx
@@ -0,0 +1,6 @@
+import { LayoutProps } from '@/models'
+import React from 'react'
+
+export function EmptyLayout({ children }: LayoutProps) {
+  return <>{children}</>
+}
---
+++ pages/api/[...path].ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import httpProxy from 'http-proxy'
+
+type Data = {
+  name: string
+}
+
+const proxy = httpProxy.createProxyServer()
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  // don't send cookies to API server
+  req.headers.cookie = ''
+
+  proxy.web(req, res, {
+    target: process.env.API_URL,
+    changeOrigin: true,
+    selfHandleResponse: false
+  })
+}
